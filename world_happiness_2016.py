9abe6a96d3d7f55b47502a1d24fd26b05d78ebe3
Jade_WKA <78435515+JadeWKA@users.noreply.github.com>
2023-06-28 16:43:14 -0400

    Update world_happiness_2016.py

--- world_happiness_2016.py
+++ world_happiness_2016.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import altair as alt
 import seaborn as sns
-import matplotlib.pyplot as plt
 
 st.header('World Happiness Data 2016 :smile:')
 
@@ -40,17 +38,3 @@ sns.heatmap(corr_df, vmin=-1, vmax=1, annot=True)
 st.write(fig)
 
 
-# st.write(df1)
-
-# st.write(df)
-
-# df2 = pd.DataFrame(
-#     np.random.randn(200,3),
-#     columns = ['a','b','c'])
-# c = alt.Chart(df2).mark_circle().encode(
-#     x = 'a', y = 'b', size = 'c', color = 'c', tooltip = ['a','b','c']
-# )
-
-# st.write(c)
-
-# sns.heatmap(df.select_dtypes(include=np.number).corr(), vmin=-1, vmax=1, annot=True, ax=ax)
