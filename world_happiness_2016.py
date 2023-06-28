aae5cd7003042d46e11f03c8dae0fb48311fb4e5
Jade_WKA <78435515+JadeWKA@users.noreply.github.com>
2023-06-28 17:07:49 -0400

    Update world_happiness_2016.py

--- world_happiness_2016.py
+++ world_happiness_2016.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import seaborn as sns
 
 st.header('World Happiness Data 2016 :smile:')
